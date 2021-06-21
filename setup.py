2b8ff665168970b0bb5bd0e6d3fbe8bcd4c06cc8
Richard Braithwaite <r.braithwaite@live.com>
2021-06-20 22:09:25 -0400

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="public-python-test-1",
-    version="0.1.0",
+    version="0.2.0",
     description="Public github dependency test.",
     author="Richard Braithwaite",
     author_email="r.braithwaite@live.com",
